0bc9b6acae9479050486cc01de90acedf26c9b73
Sam Hamm <samhamm@gmail.com>
2016-06-21 13:05:19 -0700

    after class 6/21

--- class-07-oop-constructors-tables/demo-constructors/app.js
+++ class-07-oop-constructors-tables/demo-constructors/app.js
@@ -4,7 +4,7 @@ var alison = {
   course: '201d8',
   firstName: 'Alison',
   lastInitial: 'Z',
-  faveNumber: 2,
+  faveNumber: 16,
   isCodeNinja: true,
   introduction: function() {
     console.log('My name is ' + this.firstName + ' and my favorite number is ' + this.faveNumber);
@@ -16,11 +16,34 @@ var alison = {
 
 // What if... I had a function to make these many similar objects for me? A function where I could pass in a student's unique characteristics as arguments, and then that function would make the individual objects?
 
-// Something like this would then make Jeff:
+// Something like this would then make David:
+// var alison = {
+function Student(firstName, lastInitial, faveNumber) {
+  this.course = '201d8';
+  this.lastInitial = lastInitial;
+  this.firstName = firstName;
+  this.faveNumber = faveNumber;
+  this.isCodeNinja = true;
+  this.introduction = function() {
+    console.log('My name is ' + this.firstName + ' and my favorite number is ' + this.faveNumber);
+  };
+  // console.log('This is what THIS logs:', this);
+  // alert('A object was created for ' + this.firstName);
+  mahClass.push(this);
+  // console.table(mahClass);
+}
+
+
+var mahClass = [];
+var alison = new Student('Alison', 'Z', 16);
+var david = new Student('David', 'S', 19);
+
+Student.prototype.favoriteTA = 'Nadia';
+
+alison.middleInitial = 'K';
 
 // Maybe I could take the desired object literal, and adapt it into a function that would make it?
 
 // That's a constructor function. Let's make one right here.
 
 // And what if I want somewhere to store all of these similar objects?
-// var mahClass = [];
